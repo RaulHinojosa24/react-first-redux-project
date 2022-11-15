6dfbd0da6d6c1d9d9c77deb8ab43365f20381adf
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-11-15 19:40:17 +0100

    multiple state propterties

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,25 +4,28 @@ import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
   const dispatch = useDispatch();
   const counterValue = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
-  const incrementHandler = () => {
-    console.log("increment");
-    dispatch({ type: "increment" });
+  const incrementHandler = (amount = 1) => {
+    dispatch({ type: "increment", amount: amount });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counterValue}</div>
+      {show && <div className={classes.value}>{counterValue}</div>}
       <div>
         <button onClick={decrementHandler}>Decrement</button>
-        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={() => incrementHandler(5)}>Increase by 5</button>
+        <button onClick={() => incrementHandler(1)}>Increment</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
